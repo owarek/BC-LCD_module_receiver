f1c5da0447bffc36238a9c7101d4f4171f25e7c6
owarek <owar@owar.cz>
2018-12-14 08:21:55 +0100

    Initial upload
    First upload of code. 
    LCD displays temperature sent on subscribed topic ("barometer/-/interval/set"). Radio listening is alowed only for 2 seconds after publishing of data (battery, temperature or button events)
    Known bug:
    The subscription topic works only with: "barometer/-/interval/set" I do not know why another topic can not be set, but it works for now.

---
+++ app/application.h
@@ -0,0 +1,26 @@
+#ifndef _APPLICATION_H
+#define _APPLICATION_H
+
+#ifndef VERSION
+#define VERSION "vdev"
+#endif
+
+#ifndef ROTATE_SUPPORT
+#define ROTATE_SUPPORT 1
+#endif
+
+#ifndef CORE_R
+#define CORE_R 2
+#endif
+
+#include <bcl.h>
+
+typedef struct
+{
+    uint8_t number;
+    float value;
+    bc_tick_t next_pub;
+
+} event_param_t;
+
+#endif
